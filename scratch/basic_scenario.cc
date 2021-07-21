b65218c6964de18de18d9171083cac03754843f5
KernelPanic98 <nick.toffo98@gmail.com>
2021-07-22 00:45:32 +0200

    UDP and TCP DL/UL applications added

--- scratch/basic_scenario.cc
+++ scratch/basic_scenario.cc
@@ -13,6 +13,192 @@
 #include "ns3/config-store.h"
 #include "ns3/mmwave-point-to-point-epc-helper.h"
 
+/**
+ * Application that simulates the DOWNLINK TCP data over mmWave links
+ * with the mmWave devices and the LTE EPC.
+ * Taken from: mmwave-tcp-building-example.cc
+ */
+NS_LOG_COMPONENT_DEFINE("mmWaveTCPExample");
+
+class MyAppTag : public Tag
+{
+public:
+  MyAppTag()
+  {
+  }
+
+  MyAppTag(Time sendTs) : m_sendTs(sendTs)
+  {
+  }
+
+  static TypeId GetTypeId(void)
+  {
+    static TypeId tid = TypeId("ns3::MyAppTag")
+                            .SetParent<Tag>()
+                            .AddConstructor<MyAppTag>();
+    return tid;
+  }
+
+  virtual TypeId GetInstanceTypeId(void) const
+  {
+    return GetTypeId();
+  }
+
+  virtual void Serialize(TagBuffer i) const
+  {
+    i.WriteU64(m_sendTs.GetNanoSeconds());
+  }
+
+  virtual void Deserialize(TagBuffer i)
+  {
+    m_sendTs = NanoSeconds(i.ReadU64());
+  }
+
+  virtual uint32_t GetSerializedSize() const
+  {
+    return sizeof(m_sendTs);
+  }
+
+  virtual void Print(std::ostream &os) const
+  {
+    std::cout << m_sendTs;
+  }
+
+  Time m_sendTs;
+};
+
+class MyApp : public Application
+{
+public:
+  MyApp();
+  virtual ~MyApp();
+  void ChangeDataRate(DataRate rate);
+  void Setup(Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate);
+
+private:
+  virtual void StartApplication(void);
+  virtual void StopApplication(void);
+
+  void ScheduleTx(void);
+  void SendPacket(void);
+
+  Ptr<Socket> m_socket;
+  Address m_peer;
+  uint32_t m_packetSize;
+  uint32_t m_nPackets;
+  DataRate m_dataRate;
+  EventId m_sendEvent;
+  bool m_running;
+  uint32_t m_packetsSent;
+};
+
+MyApp::MyApp()
+    : m_socket(0),
+      m_peer(),
+      m_packetSize(0),
+      m_nPackets(0),
+      m_dataRate(0),
+      m_sendEvent(),
+      m_running(false),
+      m_packetsSent(0)
+{
+}
+
+MyApp::~MyApp()
+{
+  m_socket = 0;
+}
+
+void MyApp::Setup(Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate)
+{
+  m_socket = socket;
+  m_peer = address;
+  m_packetSize = packetSize;
+  m_nPackets = nPackets;
+  m_dataRate = dataRate;
+}
+
+void MyApp::ChangeDataRate(DataRate rate)
+{
+  m_dataRate = rate;
+}
+
+void MyApp::StartApplication(void)
+{
+  m_running = true;
+  m_packetsSent = 0;
+  m_socket->Bind();
+  m_socket->Connect(m_peer);
+  SendPacket();
+}
+
+void MyApp::StopApplication(void)
+{
+  m_running = false;
+
+  if (m_sendEvent.IsRunning())
+  {
+    Simulator::Cancel(m_sendEvent);
+  }
+
+  if (m_socket)
+  {
+    m_socket->Close();
+  }
+}
+
+void MyApp::SendPacket(void)
+{
+  Ptr<Packet> packet = Create<Packet>(m_packetSize);
+  MyAppTag tag(Simulator::Now());
+
+  m_socket->Send(packet);
+  if (++m_packetsSent < m_nPackets)
+  {
+    ScheduleTx();
+  }
+}
+
+void MyApp::ScheduleTx(void)
+{
+  if (m_running)
+  {
+    Time tNext(Seconds(m_packetSize * 8 / static_cast<double>(m_dataRate.GetBitRate())));
+    m_sendEvent = Simulator::Schedule(tNext, &MyApp::SendPacket, this);
+  }
+}
+
+/*
+static void
+CwndChange (Ptr<OutputStreamWrapper> stream, uint32_t oldCwnd, uint32_t newCwnd)
+{
+	*stream->GetStream () << Simulator::Now ().GetSeconds () << "\t" << oldCwnd << "\t" << newCwnd << std::endl;
+}
+
+
+static void
+RttChange (Ptr<OutputStreamWrapper> stream, Time oldRtt, Time newRtt)
+{
+	*stream->GetStream () << Simulator::Now ().GetSeconds () << "\t" << oldRtt.GetSeconds () << "\t" << newRtt.GetSeconds () << std::endl;
+}
+*/
+
+
+static void Rx (Ptr<OutputStreamWrapper> stream, Ptr<const Packet> packet, const Address &from)
+{
+	*stream->GetStream () << Simulator::Now ().GetSeconds () << "\t" << packet->GetSize()<< std::endl;
+}
+
+/*static void Sstresh (Ptr<OutputStreamWrapper> stream, uint32_t oldSstresh, uint32_t newSstresh)
+{
+	*stream->GetStream () << Simulator::Now ().GetSeconds () << "\t" << oldSstresh << "\t" << newSstresh << std::endl;
+}*/
+
+void ChangeSpeed(Ptr<Node> n, Vector speed)
+{
+  n->GetObject<ConstantVelocityMobilityModel>()->SetVelocity(speed);
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -37,13 +223,21 @@ int main(int argc, char *argv[])
 
   //command line params
   CommandLine cmd;
-  unsigned run = 67;
+  unsigned run = 2;
   bool rlc_am = true;
+  bool UDP_DL = true;
+  bool UDP_UL = false;
+  bool TCP_DL = false;
+  bool TCP_UL = false;
   uint32_t rlc_buf_size = 10;
   uint32_t inter_pck = 200;
 
   cmd.AddValue("run", "run for RNG", run);
   cmd.AddValue("am", "RLC in AM if true", rlc_am);
+  cmd.AddValue("UDP_DL", "if true, UDP downlink traffic will be instantiated", UDP_DL);
+  cmd.AddValue("UDP_UL", "if true, UDP uplink traffic will be instantiated", UDP_UL);
+  cmd.AddValue("TCP_DL", "if true, TCP downlink traffic will be instantiated", TCP_DL);
+  cmd.AddValue("TCP_UL", "if true, TCP uplink traffic will be instantiated", TCP_UL);
   cmd.AddValue("rlc_buf_size", "size of RLC buffer [MB]", rlc_buf_size);
   cmd.AddValue("inter_pck", "inter pck interval [us]", inter_pck);
   cmd.Parse(argc, argv);
@@ -63,9 +257,9 @@ int main(int argc, char *argv[])
   //Config::SetDefault("ns3::LteRlcUm::ReportBufferStatusTimer", TimeValue(MilliSeconds(0.5)));
 
   //TODO: These lines configure the patched scheduler.
-  Config::SetDefault ("ns3::MmWaveHelper::Scheduler", StringValue("ns3::MmWaveRrIabMacScheduler"));
-  Config::SetDefault ("ns3::MmWaveIabController::CentralAllocationEnabled", BooleanValue(false));
-  Config::SetDefault ("ns3::MmWaveRrIabMacScheduler::HarqEnabled", BooleanValue(true));
+  Config::SetDefault("ns3::MmWaveHelper::Scheduler", StringValue("ns3::MmWaveRrIabMacScheduler"));
+  Config::SetDefault("ns3::MmWaveIabController::CentralAllocationEnabled", BooleanValue(false));
+  Config::SetDefault("ns3::MmWaveRrIabMacScheduler::HarqEnabled", BooleanValue(true));
 
   Config::SetDefault("ns3::MmWaveHelper::RlcAmEnabled", BooleanValue(rlc_am));
   Config::SetDefault("ns3::MmWaveRrIabMacScheduler::CqiTimerThreshold", UintegerValue(100));
@@ -73,9 +267,9 @@ int main(int argc, char *argv[])
   RngSeedManager::SetSeed(2);
   RngSeedManager::SetRun(run);
 
-  Config::SetDefault("ns3::MmWavePhyMacCommon::SymbolsPerSubframe", UintegerValue(240));
-  Config::SetDefault("ns3::MmWavePhyMacCommon::SubframePeriod", DoubleValue(1000));
-  Config::SetDefault("ns3::MmWavePhyMacCommon::SymbolPeriod", DoubleValue(1000 / 240));
+  //Config::SetDefault("ns3::MmWavePhyMacCommon::SymbolsPerSubframe", UintegerValue(240));
+  //Config::SetDefault("ns3::MmWavePhyMacCommon::SubframePeriod", DoubleValue(1000));
+  //Config::SetDefault("ns3::MmWavePhyMacCommon::SymbolPeriod", DoubleValue(1000 / 240));
 
   //mmWave module initialization and connection to the core
   Ptr<MmWaveHelper> mm_wave_helper = CreateObject<MmWaveHelper>();
@@ -102,6 +296,8 @@ int main(int argc, char *argv[])
   Ipv4AddressHelper ipv4;
   ipv4.SetBase("1.0.0.0", "255.0.0.0");
   Ipv4InterfaceContainer ip_interfaces = ipv4.Assign(internet_devices);
+  // interface 0 is localhost, 1 is the p2p device
+  Ipv4Address remote_host_addr = ip_interfaces.GetAddress(1);
 
   Ipv4StaticRoutingHelper ipv4_routing_helper;
   Ptr<Ipv4StaticRouting> remote_host_static_routing = ipv4_routing_helper.GetStaticRouting(remote_host->GetObject<Ipv4>());
@@ -195,31 +391,202 @@ int main(int argc, char *argv[])
   mm_wave_helper->AttachToClosestEnbWithDelay(ue_mm_wave_devices, possibleBaseStations, MilliSeconds(300));
 
   //TODO:  This line is needed to setup the patched scheduler. Keep here (just after the devices attachment)
-  mm_wave_helper->ActivateDonorControllerIabSetup (gnb_mm_wave_devices, iab_mm_wave_devices, 
-                                                   gnb_nodes, iab_nodes);
+  mm_wave_helper->ActivateDonorControllerIabSetup(gnb_mm_wave_devices, iab_mm_wave_devices,
+                                                  gnb_nodes, iab_nodes);
 
-  //applications setup in UE and remote hosts (downlink traffic)
-  uint16_t port = 8080;
-  ApplicationContainer client_apps;
-  ApplicationContainer server_apps;
+                                                  
+  //TCP applications setup in UE and remote hosts (downlink & uplink traffic)
+  if (TCP_DL || TCP_UL) {
+
+    uint16_t sinkPort1 = 20000;
+    uint16_t sinkPort2 = 30000;
+
+    Address sinkAddress1(InetSocketAddress(ue_ip_interfaces.GetAddress(0), sinkPort1));
+    Address sinkAddress2(InetSocketAddress(remote_host_addr, sinkPort2));
+
+    PacketSinkHelper packetSinkHelper1("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), sinkPort1));
+    PacketSinkHelper packetSinkHelper2("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), sinkPort2));
+
+    ApplicationContainer sinkApps;
+
+    if (TCP_DL && TCP_UL) {
+
+      sinkApps.Add (packetSinkHelper1.Install (ue_nodes.Get (0)));
+      sinkApps.Add (packetSinkHelper2.Install (remote_host));
+      sinkApps.Start (MilliSeconds (0.));
+	    sinkApps.Stop (MilliSeconds (1200));
+      
+      Ptr<Socket> ns3TcpSocket1 = Socket::CreateSocket (remote_host_container.Get (0), TcpSocketFactory::GetTypeId ());
+
+      Ptr<MyApp> app1 = CreateObject<MyApp>();
+      app1->Setup (ns3TcpSocket1, sinkAddress1, 1400, 5000000, DataRate ("1000Mb/s"));
+
+      remote_host_container.Get (0)->AddApplication (app1);
+
+      AsciiTraceHelper asciiTraceHelper1;
+      Ptr<OutputStreamWrapper> stream1 = asciiTraceHelper1.CreateFileStream ("mmWave-tcp-data-am-DL.txt");
+	    sinkApps.Get(0)->TraceConnectWithoutContext("Rx",MakeBoundCallback (&Rx, stream1));
+
+      app1->SetStartTime(MilliSeconds(500));
+      app1->SetStopTime(MilliSeconds(1200));
+
+      Ptr<Socket> ns3TcpSocket2 = Socket::CreateSocket (ue_nodes.Get (0), TcpSocketFactory::GetTypeId ());
+
+      Ptr<MyApp> app2 = CreateObject<MyApp>();
+      app2->Setup (ns3TcpSocket2, sinkAddress2, 1400, 5000000, DataRate ("1000Mb/s"));
+
+      ue_nodes.Get (0)->AddApplication (app2);
+      
+      AsciiTraceHelper asciiTraceHelper2;
+      Ptr<OutputStreamWrapper> stream2 = asciiTraceHelper2.CreateFileStream ("mmWave-tcp-data-am-UL.txt");
+	    sinkApps.Get(1)->TraceConnectWithoutContext("Rx",MakeBoundCallback (&Rx, stream2));
+
+      app2->SetStartTime(MilliSeconds(500));
+      app2->SetStopTime(MilliSeconds(1200));
+
+    }
+    else if (TCP_UL) {
+      
+      sinkApps.Add (packetSinkHelper2.Install (remote_host));
+      sinkApps.Start (MilliSeconds (0.));
+	    sinkApps.Stop (MilliSeconds (1200));
+
+      Ptr<Socket> ns3TcpSocket = Socket::CreateSocket (ue_nodes.Get (0), TcpSocketFactory::GetTypeId ());
+
+      Ptr<MyApp> app = CreateObject<MyApp>();
+      app->Setup (ns3TcpSocket, sinkAddress2, 1400, 5000000, DataRate ("1000Mb/s"));
+
+      ue_nodes.Get (0)->AddApplication (app);
+      
+      AsciiTraceHelper asciiTraceHelper;
+      Ptr<OutputStreamWrapper> stream1 = asciiTraceHelper.CreateFileStream ("mmWave-tcp-data-am-UL.txt");
+	    sinkApps.Get(0)->TraceConnectWithoutContext("Rx",MakeBoundCallback (&Rx, stream1));
+
+      app->SetStartTime(MilliSeconds(500));
+      app->SetStopTime(MilliSeconds(1200));
+
+    }
+    else {
+      
+      sinkApps.Add (packetSinkHelper1.Install (ue_nodes.Get (0)));
+      sinkApps.Start (MilliSeconds (0.));
+	    sinkApps.Stop (MilliSeconds (1200));
+
+      Ptr<Socket> ns3TcpSocket = Socket::CreateSocket (remote_host_container.Get (0), TcpSocketFactory::GetTypeId ());
+
+      Ptr<MyApp> app = CreateObject<MyApp>();
+      app->Setup (ns3TcpSocket, sinkAddress1, 1400, 5000000, DataRate ("1000Mb/s"));
+
+      remote_host_container.Get (0)->AddApplication (app);
+
+      AsciiTraceHelper asciiTraceHelper;
+      Ptr<OutputStreamWrapper> stream1 = asciiTraceHelper.CreateFileStream ("mmWave-tcp-data-am-DL.txt");
+	    sinkApps.Get(0)->TraceConnectWithoutContext("Rx",MakeBoundCallback (&Rx, stream1));
+
+      app->SetStartTime(MilliSeconds(500));
+      app->SetStopTime(MilliSeconds(1200));
+
+    }
+  }
   
-  UdpServerHelper packet_sink_helper(port);
-  server_apps.Add(packet_sink_helper.Install(ue_nodes.Get(0)));
-  UdpClientHelper client(ue_ip_interfaces.GetAddress(0), port);
-  client.SetAttribute("Interval", TimeValue(MicroSeconds(inter_pck)));
-  client.SetAttribute("PacketSize", UintegerValue(1400));
-  client.SetAttribute("MaxPackets", UintegerValue(0xFFFFFFFF));
-  client_apps.Add(client.Install(remote_host));
+  // UDP applications setup in UE and remote hosts (downlink & uplink traffic)
+  else
+  {
 
-  server_apps.Start(MilliSeconds(490));
-  //server_apps.Stop(Seconds(1.2));
-  client_apps.Stop(MilliSeconds(1200));
-  client_apps.Start(MilliSeconds(500));
+    uint16_t sinkPort1 = 20000;
+    uint16_t sinkPort2 = 30000;
+
+    Address sinkAddress1(InetSocketAddress(ue_ip_interfaces.GetAddress(0), sinkPort1));
+    Address sinkAddress2(InetSocketAddress(remote_host_addr, sinkPort2));
+
+    PacketSinkHelper packetSinkHelper1("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), sinkPort1));
+    PacketSinkHelper packetSinkHelper2("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), sinkPort2));
+
+    ApplicationContainer sinkApps;
+
+    if (UDP_DL && UDP_UL)
+    {
+      sinkApps.Add (packetSinkHelper1.Install (ue_nodes.Get (0)));
+      sinkApps.Add (packetSinkHelper2.Install (remote_host));
+      sinkApps.Start (MilliSeconds (0.));
+	    sinkApps.Stop (MilliSeconds (1200));
+      
+      Ptr<Socket> ns3UdpSocket1 = Socket::CreateSocket (remote_host_container.Get (0), UdpSocketFactory::GetTypeId ());
+
+      Ptr<MyApp> app1 = CreateObject<MyApp>();
+      app1->Setup (ns3UdpSocket1, sinkAddress1, 1400, 5000000, DataRate ("1000Mb/s"));
+
+      remote_host_container.Get (0)->AddApplication (app1);
+
+      AsciiTraceHelper asciiTraceHelper1;
+      Ptr<OutputStreamWrapper> stream1 = asciiTraceHelper1.CreateFileStream ("mmWave-udp-data-am-DL.txt");
+	    sinkApps.Get(0)->TraceConnectWithoutContext("Rx",MakeBoundCallback (&Rx, stream1));
+
+      app1->SetStartTime(MilliSeconds(500));
+      app1->SetStopTime(MilliSeconds(1200));
+
+      Ptr<Socket> ns3UdpSocket2 = Socket::CreateSocket (ue_nodes.Get (0), UdpSocketFactory::GetTypeId ());
+
+      Ptr<MyApp> app2 = CreateObject<MyApp>();
+      app2->Setup (ns3UdpSocket2, sinkAddress2, 1400, 5000000, DataRate ("1000Mb/s"));
+
+      ue_nodes.Get (0)->AddApplication (app2);
+      
+      AsciiTraceHelper asciiTraceHelper2;
+      Ptr<OutputStreamWrapper> stream2 = asciiTraceHelper2.CreateFileStream ("mmWave-udp-data-am-UL.txt");
+	    sinkApps.Get(1)->TraceConnectWithoutContext("Rx",MakeBoundCallback (&Rx, stream2));
+
+      app2->SetStartTime(MilliSeconds(500));
+      app2->SetStopTime(MilliSeconds(1200));
+
+    }
+    else if (UDP_UL)
+    { 
+      sinkApps.Add (packetSinkHelper2.Install (remote_host));
+      sinkApps.Start (MilliSeconds (0.));
+	    sinkApps.Stop (MilliSeconds (1200));
+
+      Ptr<Socket> ns3UdpSocket = Socket::CreateSocket (ue_nodes.Get (0), UdpSocketFactory::GetTypeId ());
+
+      Ptr<MyApp> app = CreateObject<MyApp>();
+      app->Setup (ns3UdpSocket, sinkAddress2, 1400, 5000000, DataRate ("1000Mb/s"));
+
+      ue_nodes.Get (0)->AddApplication (app);
+      
+      AsciiTraceHelper asciiTraceHelper;
+      Ptr<OutputStreamWrapper> stream1 = asciiTraceHelper.CreateFileStream ("mmWave-udp-data-am-UL.txt");
+	    sinkApps.Get(0)->TraceConnectWithoutContext("Rx",MakeBoundCallback (&Rx, stream1));
+
+      app->SetStartTime(MilliSeconds(500));
+      app->SetStopTime(MilliSeconds(1200));
+
+    }
+    else
+    { //default: UDL_DL only
+      sinkApps.Add (packetSinkHelper1.Install (ue_nodes.Get (0)));
+      sinkApps.Start (MilliSeconds (0.));
+	    sinkApps.Stop (MilliSeconds (1200));
+
+      Ptr<Socket> ns3UdpSocket = Socket::CreateSocket (remote_host_container.Get (0), UdpSocketFactory::GetTypeId ());
+
+      Ptr<MyApp> app = CreateObject<MyApp>();
+      app->Setup (ns3UdpSocket, sinkAddress1, 1400, 5000000, DataRate ("1000Mb/s"));
+
+      remote_host_container.Get (0)->AddApplication (app);
+
+      AsciiTraceHelper asciiTraceHelper;
+      Ptr<OutputStreamWrapper> stream1 = asciiTraceHelper.CreateFileStream ("mmWave-udp-data-am-DL.txt");
+	    sinkApps.Get(0)->TraceConnectWithoutContext("Rx",MakeBoundCallback (&Rx, stream1));
+
+      app->SetStartTime(MilliSeconds(500));
+      app->SetStopTime(MilliSeconds(1200));
+
+    }
+  }
+  
   mm_wave_helper->EnableTraces();
 
-  //TODO: Usually we do not use PCAPs. Instead, we use the traces enabled in the previous line
-  //      and then we parse the results (usually with a Python script).
-  //p2p.EnablePcapAll("basic-scenario");
+  //p2p.EnablePcapAll("basic_scen");
 
   Simulator::Stop(MilliSeconds(1200));
   Simulator::Run();
